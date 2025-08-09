4c677eb2ea6ef36dbd79f64a33feb97f8f9a99e6
RogerZhang888 <rogerteo200@gmail.com>
2025-08-09 18:23:11 +0800

    updates

--- script.js
+++ script.js
@@ -1,7 +1,28 @@
+const addBookButton = document.getElementById("new-book");
+
 const myLibrary = [
-  ["Harry Potter", "J. K. Rowling", true, "ca9db3e5-28e6-40cc-8a92-87367d810492"],
-  ["The Invisible Man", "H. G. Wells", false, "d215be2a-5f7a-4a31-99c5-4ec807a786f4"]
-]
+  {
+    title: "Harry Potter",
+    author: "J. K. Rowling",
+    read: true,
+    id: "ca9db3e5-28e6-40cc-8a92-87367d810492"
+  },
+  {
+    title: "The Invisible Man",
+    author: "H. G. Wells",
+    read: false,
+    id: "d215be2a-5f7a-4a31-99c5-4ec807a786f4"
+  }
+];
+
+
+document.querySelector("form").addEventListener("submit", (event) => {
+    event.preventDefault();
+    const titleValue = document.getElementById("title").value;
+    const authorValue = document.getElementById("author").value;
+    addBookToLibrary(titleValue, authorValue);
+  }
+)
 
 class Book {
   constructor(title, author) {
@@ -16,26 +37,47 @@ class Book {
 }
 
 function addBookToLibrary(title, author){
-  book = new Book(title, author);
-  bookDetails = [];
-  bookDetails.push(book.title).push(book.author).push(book.read).push(book.id);
-  myLibrary.push(bookDetails);
-}
-
-function addRow(tableID){
-  let tableRef = document.getElementById(tableID);
-  let newRow = tableRef.insertRow(-1);
-  newRow.insertCell(0);
-  newRow.insertCell(1);
-  newRow.insertCell(2);
-  newRow.insertCell(3);
+  let book = new Book(title, author);
+  myLibrary.push(book);
+  console.log(book);
+  console.log(myLibrary);
+  displayBooks(myLibrary);
 }
 
 function displayBooks(myLibrary){
-  for (let i in myLibrary){
-    addRow(library);
-    for (let j in i){
-      table.row[-1].cell[j].innerHTML = j; 
-    }
-  }
+  const table = document.getElementById("all-books")
+  table.innerHTML = "";
+  myLibrary.forEach(book => {
+    newRow = document.createElement("tr")
+
+    let titleCell = document.createElement("td")
+    titleCell.innerHTML = book.title
+    newRow.appendChild(titleCell)
+
+    let authorCell = document.createElement("td")
+    authorCell.innerHTML = book.author
+    newRow.appendChild(authorCell)
+
+    let readCell = document.createElement("td")
+    readCell.innerHTML = book.read
+    newRow.appendChild(readCell)
+
+    let idCell = document.createElement("td")
+    idCell.innerHTML = book.id
+    newRow.appendChild(idCell)
+
+    let removeCell = document.createElement("td")
+    removeCell.innerHTML = "<button>Remove</button>";
+    newRow.appendChild(removeCell)
+
+    table.appendChild(newRow);
+  });
 }
+
+addEventListener(
+  "DOMContentLoaded", (event) => {
+    displayBooks(myLibrary);
+  }
+)
+
+
